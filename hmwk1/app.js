20b8d4618856f2524c75c6652867a651a7bedccb
asiri96 <asiri1siri@gmail.com>
2018-03-11 16:05:24 -0700

    fixed error

---
+++ hmwk1/app.js
@@ -0,0 +1,30 @@
+// Fixed Errors from LAB 01 and LAB 02 Comments //
+// Now has working PubSub //
+/*
+class PubSub {
+    constructor () {
+        this.subscribers = [];
+    }
+    subscribe (fn) {
+        this.subscribers.push(fn);
+    }
+
+    publish (data) {
+        this.subscribers.forEach(subscriber => {
+            subscriber(data);
+        });
+    }
+}
+
+const pubSub = new PubSub();
+
+pubSub.subscribe(data => {
+    console.log(data);
+    window.incrementalGame.state.counter += data;
+});
+
+var pickDaCorn = document.querySelector('#pick').addEventListener('click', function(){
+      pubSub.publish(1);
+      document.querySelector('#cornNum').innerHTML = window.incrementalGame.state.counter;
+    });
+    */
--- lab3/client/src/constants.js
+++
@@ -1,8 +0,0 @@
-export default {
-	growthRatio: 0.05,
-	actions: {
-		EXAMPLE: 'EXAMPLE_MUTATION',
-		CORN_PICK: 'CORN_PICK',
-		BUY_GENERATOR: 'BUY_GENERATOR'
-	}
-};
