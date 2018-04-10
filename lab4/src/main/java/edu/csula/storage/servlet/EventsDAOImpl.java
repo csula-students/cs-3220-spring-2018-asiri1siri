5db547b5e03f45d9fcf3515a33c502b6435e1645
asiri96 <asiri1siri@gmail.com>
2018-04-09 19:49:12 -0700

    100% test success

--- lab4/src/main/java/edu/csula/storage/servlet/EventsDAOImpl.java
+++ lab4/src/main/java/edu/csula/storage/servlet/EventsDAOImpl.java
@@ -6,8 +6,8 @@ import java.util.Optional;
 
 import javax.servlet.ServletContext;
 
-import edu.csula.storage.EventsDAO;
 import edu.csula.models.Event;
+import edu.csula.storage.EventsDAO;
 
 /**
  * To abstract the storage access from the business layer using ServletContext
@@ -38,30 +38,85 @@ public class EventsDAOImpl implements EventsDAO {
 	}
 
 	@Override
-	public List<Event> getAll() {
+	public List<Event> getAll() 
+	{
 		// TODO: read a list of events from context
-		return new ArrayList<>();
+		
+		ArrayList<Event> EV = new ArrayList<Event>(); 
+		EV = (ArrayList<Event>) context.getAttribute(CONTEXT_NAME);
+		
+		if(EV == null)
+		{
+			return new ArrayList<>(); //empty list
+		}
+		
+		return EV;
 	}
 
 	@Override
-	public Optional<Event> getById(int id) {
+	public Optional<Event> getById(int id) 
+	{
 		// TODO: get a certain event given its id from context (see getAll() on
 		// getting a list first and get a certain one from the list)
+		
+		ArrayList<Event> EV = (ArrayList<Event>) getAll();
+		
+		for(int x = 0; x < EV.size(); x++)
+		{
+			if(EV.get(x).getId() == id)
+			{
+				return Optional.of(EV.get(x));
+			}
+		}
+		
 		return Optional.empty();
 	}
 
 	@Override
-	public void set(int id, Event event) {
+	public void set(int id, Event event) 
+	{
 		// TODO: set a certain event given id to be different from context
+		
+		ArrayList<Event> EV = (ArrayList<Event>) getAll();
+		
+		for(int x = 0; x < EV.size(); x++)
+		{
+			if(EV.get(x).getId() == id)
+			{
+				EV.set(x, event); 
+			}
+		}
+	
+		context.setAttribute(CONTEXT_NAME, EV);
 	}
 
 	@Override
-	public void add(Event event) {
+	public void add(Event event) 
+	{
 		// TODO: add a new event to the context
+		
+		ArrayList<Event> EV = (ArrayList<Event>) getAll();
+		EV.add(event);
+		
+		context.setAttribute(CONTEXT_NAME, EV);
 	}
+	
 
 	@Override
-	public void remove(int id) {
+	public void remove(int id) 
+	{
 		// TODO: remove a single event given id
+		
+		ArrayList<Event> EV = (ArrayList<Event>) getAll();
+		
+		for(int x = 0; x < EV.size(); x++)
+		{
+			if(EV.get(x).getId() == id)
+			{
+				EV.remove(x);
+			}
+		}
+		
+		context.setAttribute(CONTEXT_NAME, EV);
 	}
 }
